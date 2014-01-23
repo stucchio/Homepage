b72150120776bc93e2e08f045d938c504757c3a7
Chris Stucchio <stucchio@gmail.com>
2014-01-23 05:58:16 -0500

    minor page speed updates

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 rm -r content/blog/tags/ deploy
-./pyenv/bin/hyde gen
+hyde gen
 s3cmd -c s3cfg sync deploy/* s3://www.chrisstucchio.com
+s3cmd -c s3cfg --add-header='Cache-Control:max-age=604800, public' put --recursive deploy/media/* s3://www.chrisstucchio.com/media/
+s3cmd -c s3cfg sync deploy/* s3://chrisstucchio.com
+s3cmd -c s3cfg --add-header='Cache-Control:max-age=604800, public' sync deploy/media/* s3://chrisstucchio.com/media/
 rm -r deploy
