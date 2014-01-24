2769b5033a0cab06d55568144ead5fce6be2452f
Chris Stucchio <stucchio@gmail.com>
2014-01-24 03:23:25 -0500

    updates to deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,15 +6,21 @@ hyde gen
 find deploy/media/ -name '*.css' -or -name '*.js' | xargs -I filename yui-compressor filename -o filename.min
 find deploy/media/ -name '*.min' | sed s/\\.min// | xargs -I filename mv filename.min filename
 # Compress
-find deploy/media/ -name '*.css' -or -name '*.js' | xargs gzip -9
-find deploy/media/ -name '*.gz' | sed s/\\.gz// | xargs -I filename mv filename.gz filename
+find deploy/ -name '*.css' -or -name '*.js' -or -name '*.html' | xargs -I filename gzip -9 "filename"
+find deploy/ -name '*.gz' | sed s/\\.gz// | xargs -I filename mv "filename.gz" "filename"
+
+# Optimize
+find deploy/ -name '*.jpg' | xargs -I filename jpegoptim --strip-all filename
 
 #Upload
-s3cmd -c s3cfg sync deploy/* s3://www.chrisstucchio.com
+s3cmd -c s3cfg --exclude '*.html' --exclude '*.css' --exclude '*.js' sync deploy/* s3://www.chrisstucchio.com
 #Upload media with cache headers
-s3cmd -c s3cfg --add-header='Cache-Control:max-age=604800, public' --add-header='Content-Encoding:gzip' put --recursive deploy/media/* s3://www.chrisstucchio.com/media/
+find deploy/ -name '*.css' -or -name "*.js" -or -name "*.html" | sed s/^deploy\\/// |xargs -I filename s3cmd -c s3cfg --add-header='Content-Encoding:gzip' put deploy/filename s3://www.chrisstucchio.com/filename
 
 #Repeat for non-www
-s3cmd -c s3cfg sync deploy/* s3://chrisstucchio.com
-s3cmd -c s3cfg --add-header='Cache-Control:max-age=604800, public' --add-header='Content-Encoding:gzip' put --recursive deploy/media/* s3://chrisstucchio.com/media/
+s3cmd -c s3cfg --exclude '*.html' --exclude '*.css' --exclude '*.js' sync deploy/* s3://chrisstucchio.com
+#Upload media with cache headers
+find deploy/ -name '*.css' -or -name "*.js" -or -name "*.html" | sed s/^deploy\\/// |xargs -I filename s3cmd -c s3cfg --add-header='Content-Encoding:gzip' put deploy/filename s3://chrisstucchio.com/filename
+
+
 rm -r deploy
