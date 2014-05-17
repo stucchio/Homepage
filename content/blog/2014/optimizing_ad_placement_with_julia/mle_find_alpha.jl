fc2d211534ae7d5c2dc55f8330d31fffddb1c5a4
Chris Stucchio <stucchio@gmail.com>
2014-05-17 13:30:42 +0530

    Ad placement post with julia is more or less ready.

--- content/blog/2014/optimizing_ad_placement_with_julia/mle_find_alpha.jl
+++ content/blog/2014/optimizing_ad_placement_with_julia/mle_find_alpha.jl
@@ -19,14 +19,26 @@ function logLikelihood(z::Array{Float64,1}, clicks::Array{Int64,2}, shows::Array
     return result
 end
 
-function gradAlphaLogLikelihood(z::Array{Float64,1}, clicks::Array{Int64,2}, shows::Array{Int64,2}, alpha::Array{Float64,1})
+function betaToAlpha(beta::Array{Float64,1})
+    result = Array(Float64, size(beta)[1] + 1)
+    result[1] = 1.0
+    result[2:] = cumprod(beta)
+    return result
+end
+
+function gradBetaLogLikelihood(z::Array{Float64,1}, clicks::Array{Int64,2}, shows::Array{Int64,2}, beta::Array{Float64,1})
+    alpha = betaToAlpha(beta)
     M, N = size(clicks)
     @assert size(z) == (M,)
     @assert size(alpha) == (N,)
-    result = Array(Float64, N)
-    alpha[1] = 1.0
-    for j=1:M
-        result +=  (slice(clicks, j,:) ./ alpha)  .- (slice(shows, j,:) .- slice(clicks, j,:)) .* alpha ./ (1 .- alpha * z[j])
+    result = Array(Float64, N-1)
+
+    for k=2:N
+        for i=k:N
+            for j=1:M
+                result[k-1] += clicks[j,i] / beta[k-1] - (shows[j,i] - clicks[j,i]) * z[j] * (alpha[i] / beta[k-1]) / (1 - alpha[i]*z[j])
+            end
+        end
     end
     return result
 end
@@ -34,7 +46,6 @@ end
 function gradZLogLikelihood(z::Array{Float64,1}, clicks::Array{Int64,2}, shows::Array{Int64,2}, alpha::Array{Float64,1})
     M, N = size(clicks)
     result = Array(Float64, M)
-    alpha[1] = 1.0
     for i=1:N
         result += ( (slice(clicks, :, i) ./ z)  .- (((slice(shows, :, i) .- slice(clicks, :, i)) .* alpha[i]) ./ (1 .- alpha[i] * z)) )
     end
@@ -59,33 +70,32 @@ function computeAlphaZ(clicks::Array{Int64,2}, shows::Array{Int64,2})
     # Here the y-variable is an M+N-dimensional Array{Float64,1} the first N represent transformed
     # alpha, the remainder represent transformed z
     function f(y::Array{Float64,1})
-        alpha = map(yToZ, y[1:N])
-        alpha[1] = 1.0
-        z = map(yToZ, y[N+1:N+M])
+        z = map(yToZ, y[1:M])
+        beta = map(yToZ, y[M+1:M+N-1])
+        alpha = betaToAlpha(beta)
         return -1*logLikelihood(z, clicks, shows, alpha)
     end
 
     function df!(y::Array{Float64,1}, storage::Array{Float64,1})
-        alpha = map(yToZ, y[1:N])
-        alpha[1] = 1.0
-        z = map(yToZ, y[N+1:N+M])
-        storage[1:N] = -1*gradAlphaLogLikelihood(z, clicks, shows, alpha) .* map( dzdy, y[1:N])
-        storage[N+1:N+M] = -1*gradZLogLikelihood(z, clicks, shows, alpha) .* map(dzdy, y[N+1:N+M])
-        storage[1] = 0.0
+        z = map(yToZ, y[1:M])
+        beta = map(yToZ, y[M+1:M+N-1])
+        alpha = betaToAlpha(beta)
+        storage[1:M] = -1*gradZLogLikelihood(z, clicks, shows, alpha) .* map(dzdy, y[1:M])
+        gb = gradBetaLogLikelihood(z, clicks, shows, beta)
+        storage[M+1:M+N-1] = -1*gradBetaLogLikelihood(z, clicks, shows, beta) .* map( dzdy, y[M+1:M+N-1])
     end
-    init = Array(Float64, M+N)
+    init = Array(Float64, M+N-1)
     init[:] = 0.0
     result = optimize(f, df!, init, method = :gradient_descent)
     resultUntransformed = map( yToZ, result.minimum)
-    alpha = resultUntransformed[1:N]
-    alpha[1] = 1.0
-    z = resultUntransformed[N+1:N+M]
+    alpha = betaToAlpha(resultUntransformed[M+1:M+N-1])
+    z = resultUntransformed[1:M]
     return (alpha, z)
 end
 
-clicks = [ 25 13 10; 20 9 4; 10 3 0; 12 4 1]
+clicks = [ 25 13 10; 20 9 4; 10 4 1; 12 4 0]
 shows = [ 100 105 97; 99 103 96; 102 100 101; 103 101 107]
-z = [0.25, 0.2, 0.1, 0.1]
-alpha = [1.0, 0.5, 0.25]
 
 println(computeAlphaZ(clicks, shows))
+
+(alpha, z) = computeAlphaZ(clicks, shows)
